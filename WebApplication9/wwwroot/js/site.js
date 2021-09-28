4f2390da5e8f32b908abd46061323f67779a435f
vitralik <75981679+coolcakev@users.noreply.github.com>
2021-09-28 19:41:08 +0300

    two

--- WebApplication9/wwwroot/js/site.js
+++ WebApplication9/wwwroot/js/site.js
@@ -2,3 +2,18 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+
+    $("input[type='submit']").click(function (e) {
+        e.preventDefault();
+        var controler = $(this).attr("con");
+        var number = $(this).siblings("#number").val();
+
+        controler = encodeURIComponent(controler);
+
+        number = encodeURIComponent(number);
+
+
+        $('#results').load("https://localhost:44369/" + controler + "/result?number=" + number);
+    });
+});
